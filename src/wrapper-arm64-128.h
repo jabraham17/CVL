720ea851c505bdbcc093b172d3e5f83c5339601c
Jade Abraham <jade.abraham@hpe.com>
2024-10-22 19:53:46 -0700

    add missing arm function
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- src/wrapper-arm64-128.h
+++ src/wrapper-arm64-128.h
@@ -6,3 +6,8 @@
 #include "wrapper-arm64-128-int64.h"
 #include "wrapper-arm64-128-real32.h"
 #include "wrapper-arm64-128-real64.h"
+
+
+// for whatever reason, these are missing from the arm_neon.h header
+static inline uint64x2_t vmvnq_u64(uint64x2_t v) { return ~v; }
+static inline int64x2_t vmvnq_s64(int64x2_t v) { return ~v; }

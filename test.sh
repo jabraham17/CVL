0f7d78b4eb1be1fd6f3f5926b6a4bf7e28c6f9f9
Jade Abraham <jade.abraham@hpe.com>
2025-02-26 12:47:57 -0600

    fix test script
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- test.sh
+++ test.sh
@@ -6,4 +6,13 @@ FILE_DIR=$(cd $(dirname $0); pwd)
   mason test --show -- \
     $@ \
     $(./compile.py --arch-compopts) \
+  | tee $FILE_DIR/test.log \
 )
+
+# check the last line of test.log, if it starts with OK, then the test passed
+# this is required because mason exits with 1 if a test is skipped
+if [[ "$(tail -n 1 $FILE_DIR/test.log)" == "OK"* ]]; then
+  exit 0
+else
+  exit 1
+fi

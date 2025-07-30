56afc481360568f6b2fb001b73e0b044912851f9
Jade Abraham <jade.abraham@hpe.com>
2025-07-30 07:34:09 -0600

    use Mason.toml instead of .chplcheck.cfg
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/lint.sh
+++ util/lint.sh
@@ -4,8 +4,5 @@ PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 set -e
 
-(set -x && chplcheck -c $PROJECT_DIR/.chplcheck.cfg \
-  "$PROJECT_DIR/src/**/*.chpl" \
-  "$PROJECT_DIR/test/**/*.chpl"
-  $@ \
-)
+(set -x && chplcheck -c $PROJECT_DIR/Mason.toml $@)
+

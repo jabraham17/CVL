ff26d3b2f8e0d4bf984673b64544099659f8d93c
Jade Abraham <jade.abraham@hpe.com>
2025-09-21 22:06:54 -0700

    fix unused arg on macos
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- compile.py
+++ compile.py
@@ -22,6 +22,9 @@ def get_arch():
         return "arm64"
     return machine
 
+def get_os():
+    platform = os.uname()[0]
+    return platform.lower()
 
 class Project:
     def __init__(self, workspace):
@@ -52,8 +55,9 @@ class Project:
                 arch_compopts += (
                     " --set useSLEEF --set SLEEF_INSTALL='{}'".format(sleef_dir)
                 )
-                # FIXME: sleef static libs are not PIE, so disable PIE for now
-                arch_compopts += " --ccflags -no-pie --ldflags -no-pie"
+                if get_os() == "linux":
+                    # FIXME: sleef static libs are not PIE, so disable PIE for now
+                    arch_compopts += " --ccflags -no-pie --ldflags -no-pie"
             else:
                 install_script = (
                     self.workspace / "third-party" / "sleef" / "install.sh"
